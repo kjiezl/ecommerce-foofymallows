184abc83b838e16e05d20107ff2a0285d29904cc
Kwenn Jiezl <kjiezl08@gmail.com>
2025-01-29 08:33:32 +0800

    updated vite.config and .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
-    proxy: {
+    proxy: mode === 'development' ? {
       '/api': 'http://localhost:5000',
-      '/uploads/': 'http://localhost:5000'
-    }
-  }
-})
+      '/uploads/': 'http://localhost:5000',
+    } : undefined,
+  },
+}));
